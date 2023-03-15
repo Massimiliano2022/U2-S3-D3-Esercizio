ae92b5f41a7936653d04c8c362132710f7170efe
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-15 20:47:37 +0100

    modifiche esercizio

--- js/script.js
+++ js/script.js
@@ -3,40 +3,35 @@ fetch("https://striveschool-api.herokuapp.com/books")
     .then(responseObj => responseObj.json())
 
     .then(books => {
-        const grid = document.getElementById("books-container")
+
+        const grid = document.getElementById("grid")
         grid.innerHTML = ""
 
-        for (let i = 0; i < books.length; i++) {
-            // crea una nuova riga ogni quattro libri
-            // se i / 4 è uguale a 0 creo una nuova riga
-            if (i % 4 === 0) {
-                const row = document.createElement("div")
-                row.className = "row"
-                grid.appendChild(row)
-            }
+        books.forEach((book) => {
 
-            // crea una nuova colonna per il libro corrente
             const col = document.createElement("div")
-            col.className = "col-6 col-md-3"
+            col.className = "col"
             col.innerHTML = `
             <div class="card mx-2 my-2">
-                <img src="${books[i].img}" class="card-img-top img-fluid" alt="Copertina">
+                <div class="card-img" style="background-image: url('${book.img}')">
+                    
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title">${books[i].title}</h5>
-                    <p class="card-text">Price: ${books[i].price}</p>
+                    <h5 class="card-title text-truncate">${book.title}</h5>       
+                    <p class="card-text">Price: ${book.price}</p>
                     <button type="button" class="btn btn-primary" id="btnDelete">Delete</button>
                 </div>
             </div>
             `
+            //<img src="${book.img}" class="object-fit-cover img-fluid" alt="Copertina">
+            
             const deleteCard = col.querySelector("#btnDelete")
             deleteCard.onclick = () => {
                 col.remove();
             }
 
-            // aggiungi la colonna alla riga corrente
-            const currentRow = grid.lastChild
-            currentRow.appendChild(col)
-        }
+            grid.appendChild(col);
+        })
 
     })
     .catch(error => console.log("CATCH", error))
