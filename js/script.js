3848e442cd9cb2c1d7bf23752e22a8aa34d2703d
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-16 11:43:11 +0100

    implementazione carrello e rimuovi dal carrello

--- js/script.js
+++ js/script.js
@@ -21,38 +21,29 @@ fetch("https://striveschool-api.herokuapp.com/books")
                     <h5 class="card-title text-truncate" id="book-title">${book.title}</h5>       
                     <p class="card-text" id="book-price">Price: ${book.price}</p>
                     <button type="button" class="btn btn-primary" id="btnDelete">Delete</button>
-                    <button class="btn btn-primary" id="btnAddShopping" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                    <button class="btn btn-primary" id="btnAddShopping" type="button" data-bs-toggle="offcanvas" data-bs-target="#cart" aria-controls="cart">
                         Add to shopping-cart
                     </button>
-                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="cart" aria-labelledby="cartLabel">
                         <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                            <h5 class="offcanvas-title" id="cartLabel">Cart</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
-                            <div>
-                                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                            </div>
-                            <div class="dropdown mt-3">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                                    Dropdown button
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
             `
-            
+
             const deleteCard = col.querySelector("#btnDelete")
             deleteCard.onclick = () => {
                 col.remove();
             }
+
+
+
             const addShopping = col.querySelector("#btnAddShopping")
             addShopping.onclick = () => {
                 let bookTitle = addShopping.closest(".card-body").querySelector("#book-title").innerText;
@@ -60,10 +51,37 @@ fetch("https://striveschool-api.herokuapp.com/books")
                 let cartItem = { title: bookTitle, price: bookPrice };
                 shoppingCart.push(cartItem);
                 localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+                const cart = document.querySelector("#cart .offcanvas-body");
+                const cartItemDiv = document.createElement("div");
+                cartItemDiv.classList.add("card");
+                cartItemDiv.innerHTML = `
+                    
+                        <div class="card-body">
+                            <h5 class="card-title text-truncate">${cartItem.title}</h5>
+                            <p class="card-text">${cartItem.price}</p>
+                            <button type="button" class="btn btn-primary" id="removeItemsFromCart">Remove from cart</button>
+                        </div>
+                    `;
+
+                cart.appendChild(cartItemDiv);
+
+                // Aggiungi evento "click" a tutti i pulsanti "Remove from cart"
+                const removeBtns = document.querySelectorAll("#cart .offcanvas-body .card #removeItemsFromCart");
+                removeBtns.forEach((btn) => {
+                    btn.onclick = () => {
+                        let bookTitle = btn.closest(".card").querySelector(".card-title").innerText;
+                        let cartItemIndex = shoppingCart.findIndex(item => item.title === bookTitle);
+                        if (cartItemIndex > -1) {
+                            shoppingCart.splice(cartItemIndex, 1);
+                            localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+                            btn.closest(".card").remove();
+                        }
+                    };
+                });
+
             }
 
             grid.appendChild(col);
         })
-
     })
     .catch(error => console.log("CATCH", error))
